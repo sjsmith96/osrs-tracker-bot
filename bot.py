42d96b600472cb472ec157c24748e123262d7eaf
EddieSpaghetti666 <Dsmith141996@gmail.com>
2021-12-21 23:07:48 -0500

    hello bot

---
+++ bot.py
@@ -0,0 +1,33 @@
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+
+from discord.ext import commands
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.command(name='create-channel')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name='real-python'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+bot.run(TOKEN)
+
