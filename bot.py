21c39793d053e82cadb55762ffd66e2dfa6cfb57
EddieSpaghetti666 <Dsmith141996@gmail.com>
2021-12-22 00:19:48 -0500

    hacked

--- bot.py
+++ bot.py
@@ -15,14 +15,9 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!')
 
 
-@bot.command(name='create-channel')
-@commands.has_role('admin')
-async def create_channel(ctx, channel_name='real-python'):
-    guild = ctx.guild
-    existing_channel = discord.utils.get(guild.channels, name=channel_name)
-    if not existing_channel:
-        print(f'Creating a new channel: {channel_name}')
-        await guild.create_text_channel(channel_name)
+@bot.command(name='greeting')
+async def create_channel(ctx):
+    await ctx.send("Hello!")
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -30,4 +25,6 @@ async def on_command_error(ctx, error):
         await ctx.send('You do not have the correct role for this command.')
 
 bot.run(TOKEN)
+
+
 
